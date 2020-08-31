ccdcb0847e3e838f81369676bdbeb7858a06e3b7
dendrebeden <shushpanovdenis@gmail.com>
2020-09-01 01:00:22 +0200

    Task 4 solved: Sex and Embarked columns are substitute by dummy columns

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,6 +1,14 @@
 import pandas as pd
 import numpy as np
 
+def age_misssing(cols, age_dict):
+    Pclass = cols[0]
+    Age = cols[1]
+    if pd.isnull(Age):
+        return age_dict[Pclass]
+    else:
+        return Age
+
 def preprocess(input_file, output_file):
     """Removes 'Name', 'Ticket' and 'Cabin' columns from input file
 
@@ -12,7 +20,10 @@ def preprocess(input_file, output_file):
     """
     # Import data from input_file.
     df = pd.read_csv(input_file, sep = ";")
-    df = df.dropna()
+    # Substitute 'Age' column values with mean by 'Pclass'.
+    age_mean_by_pclass = df.groupby(by=["Pclass"]).mean()["Age"]
+    
+    df["Age"] = df[["Pclass", "Age"]].apply(lambda x: age_misssing(x, age_mean_by_pclass), axis = 1)
 
     # Drop columns where features are making no sense.
     df.drop(["Name", "Ticket", "Cabin", "PassengerId"], inplace = True, axis = 1)
