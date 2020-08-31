eff7dd7cd68214321b62f4e6d04043a3f4206a9c
dendrebeden <shushpanovdenis@gmail.com>
2020-08-31 23:15:05 +0200

    Task 3 solution

--- src/build_features.py
+++ src/build_features.py
@@ -1,32 +1,37 @@
 import pandas as pd
 
-import sklearn
-
-def execute(input_file, output_file, force_write = True):
-    """Builds features
+def build_features(input_file, output_file):
+    """Function substitutes 'Sex' and 'Embarked' columns values with dummies and creates
+       'FamilySize' and 'IsAlone' columns
 
     Args:
-        input_file (str): input file.
+        input_file : string
+            csv input file name
+        output_file : string
+            csv output file name
     """
+    # Import data from input_file.
     df = pd.read_csv(input_file)
+    assert(all([item in df.columns for item in ["Sex", "Embarked", "SibSp", "Parch"]]))
 
+    # Substitute 'Sex' column values with dummies.
     df["Sex"] = df["Sex"].replace("male", 0)
     df["Sex"] = df["Sex"].replace("female", 1)
-
+    
+    # Substitute 'Embarked' column values with dummies.
     embarked_dict = {}
     embarked_dict_values = 0
-    for i in df.Embarked:
-        if i in embarked_dict.keys():
-            pass
-        else:
-            embarked_dict_values = embarked_dict_values + 1
-            embarked_dict[i] = embarked_dict_values
-    
+    for i in df.Embarked.unique():
+        embarked_dict_values = embarked_dict_values + 1
+        embarked_dict[i] = embarked_dict_values
+
     for i in embarked_dict.keys():
         df["Embarked"].replace(i, embarked_dict[i], inplace = True)
 
+    # Calculate family size column.
     df["FamilySize"] = df["SibSp"] + df["Parch"] + 1
-    df["IsAlone"] = 0
-    df.loc[df["FamilySize"] == 1, "IsAlone"] = 1
-
-    df.to_csv(output_file)
+    # Define alone passengers
+    df["IsAlone"] = df["FamilySize"].apply(lambda x: x == 1 if 1 else 0)
+    
+    # Export data to output_file.
+    df.to_csv(output_file, index = False)
--- src/predict.py
+++ src/predict.py
@@ -1,25 +1,28 @@
 import pandas as pd
 import pickle as pkl
+from build_features import build_features 
+from sklearn.metrics import classification_report
+from preprocess import preprocess
 
-df = pd.read_csv("data/val_bf.csv")
+# Preprocess and prepare data.
+preprocess("../data/val.csv", "../data/val_.csv")
+build_features("../data/val_.csv", "../data/val_.csv")
+# Import validation data.
+df = pd.read_csv("../data/val_.csv")
+assert(all([item in df.columns for item in ["Survived", "Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked"]]))
 
-df.dropna(inplace = True)
-
-target = df["Survived"]
-del(df["Survived"])
-    
+# Create 'data/model.pkl' file if it does not exist.
+dirname = os.path.dirname("data/model.pkl")
+if not os.path.exists(dirname):
+    os.makedirs(dirname)
+# Read model from 'data/model.pkl'.
 model_unpickle = open("data/model.pkl", 'rb')
 model = pkl.load(model_unpickle)
-model.close()
+model_pickle.close()
 
-predictions = model.predict(df)
+# Predict results.
+pred = model.predict(df.drop("Survived", axis = 1))
 # Reassign target (if it was present) and predictions.
-df["prediction"] = predictions
-df["target"] = target
+metric_result = sklearn.metrics.accuracy_score(df["Survived"], pred)
 
-ok = 0
-for i in df.iterrows():
-    if (i[1]["target"] == i[1]["prediction"]):
-        ok = ok + 1
-
-print("accuracy is", ok / df.shape[0])
+print("train_accuracy for the model is " + str(metric_result))
